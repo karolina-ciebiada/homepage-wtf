806cff4928ed44db274779cfdb418aa6c7d5ccfa
Karolina Ciebiada <karolina.ciebiada@icloud.com>
2020-05-25 10:40:34 +0200

    new structure

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
-const name='Karo'
+const name='Karo';
 const age=19;
 
-console.log(name);
-console.log(age);
-console.log(`nazywam się ${name} i mam ${age} lat.`)
+console.log(
+    `nazywam się ${name} i mam ${age} lat.`)
+
+const heading = document.querySelector('.main-heading');
+
+heading.innerHTML = `SZKOLENIE WTF`
+
+const footer = document.querySelector('.footer--js');
+
+footer.innerHTML = `TO JEST STOPKA, KTÓRĄ ZMIENIAM JAVA SCRIPTEM.`;
+
+const greet = (age, name) => {
+    console.log(`Akuku ${name}`);
+}
+greet(age,name);
+
+function newGreet(age, name){
+    console.log(`Mam na imię ${name} i mam ${age} lat`);
+}
+newGreet(age,name);
